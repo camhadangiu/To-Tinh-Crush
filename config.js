444cc8ae478111ae496674849f01c58b7f5b96f5
camhadangiu <87258327+camhadangiu@users.noreply.github.com>
2021-07-11 14:28:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,13 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    introTitle: 'hello ank!'
+    title: 'ank có iu em khong:(',
+    desc: 'thoát ra là dồng ý iu em nkaaa',
+    btnYes: 'yêu em vailon hehe',
+    btnNo: 'còn lâu :) :3',
+    question:'đcm tóm lại là yêu em nkaaa <3',
+    btnReply: 'Gửi',
+    reply: 'uki yêu em vailon <3',
+    mess: 'em biết mà 🥰. Yêu ank vcl 😘😘',
+    messDesc: 'xem đến đây rồi thì ib em đyy',
+    btnAccept: 'Okiiiii lun ;3',
+    messLink: 'https://www.facebook.com/camhadangiu
